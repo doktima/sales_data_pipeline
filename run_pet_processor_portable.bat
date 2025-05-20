28249bbf061a61fd1393c5d151f84d2913664193
Tema Dokturbek Uulu <mutimakg@gmail.com>
2025-05-20 19:06:14 +0100

    Update run_pet_processor_portable.bat

--- run_pet_processor_portable.bat
+++ run_pet_processor_portable.bat
@@ -22,17 +22,17 @@ if not exist "%PYTHON_PATH%" (
 REM Ask for team member 
 echo Please select your name:
 echo.
-echo    [1] Asa
-echo    [2] Natalja
-echo    [3] Shyam
-echo    [4] Tima
+echo    [1] (Team Member 1)
+echo    [2] (Team Member 2)
+echo    [3] (Team Member 3)
+echo    [4] (Team Member 4)
 echo.
 choice /c 1234 /n /m "Enter number (1-4): "
 
-if %ERRORLEVEL%==1 set MEMBER_DIR=Asa
-if %ERRORLEVEL%==2 set MEMBER_DIR=Natalja
-if %ERRORLEVEL%==3 set MEMBER_DIR=Shyam
-if %ERRORLEVEL%==4 set MEMBER_DIR=Tima
+if %ERRORLEVEL%==1 set MEMBER_DIR=(Team Member 1)
+if %ERRORLEVEL%==2 set MEMBER_DIR=(Team Member 2)
+if %ERRORLEVEL%==3 set MEMBER_DIR=(Team Member 3)
+if %ERRORLEVEL%==4 set MEMBER_DIR=(Team Member 4)
 
 echo.
 echo Processing PET forms for %MEMBER_DIR%...
